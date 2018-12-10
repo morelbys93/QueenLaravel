75b5ecf18770b5bf5db770119426ca9acf16f537
morelbys93 <rodriguezmorel@hotmail.com>
2018-12-10 11:08:22 -0300

    funcion create y deteccion de errores en el form

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,4 +18,27 @@ class ProductController extends Controller
         return view('product', compact('products' ));
     }
 
+
+    public function create(Request $request){
+      $this->validate($request,[
+        'name'=>'required|string|max: 60|',
+        'description'=>'required|max:255|',
+        'image'=>'image',
+        'price'=>'required',
+        'type'=>'required',
+        'promoted'=>'required',
+        'slider'=> 'required',
+
+      ]);
+
+      $product= new Product;
+        $product->name = $request['name'];
+        $product->description = $request['description'];
+        $product->image = $request['image'];
+        $product->price = $request['price'];
+        $product->type = $request['type'];
+        $product->promoted = $request['promoted'];
+        $product->slider = $request['slider'];
+
+      }
 }
