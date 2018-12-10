0b6d0f68d44ea6f936e4278cb97136f76f584f8a
ale-vivone <vivoneaj@gmail.com>
2018-12-10 10:51:54 -0300

    ajustes en Factory de producto y llamada a foto en vista.

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -4,12 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Product::class, function (Faker $faker) {
     $type_ = ["dulce","salado"]; // clasificación de los productos. // TODO: Hacerlo por tabla.
-    $path = storage_path('app/public/uploads'); //carga las imágenes fake un la carpeta /uploads dentro de storage.
+    $path = storage_path('/app/public/uploads'); //carga las imágenes fake un la carpeta /uploads dentro de storage.
 
     return [
       'name' => $faker->sentence(3),
       'description' => $faker->sentence(20),
-      'image'=> $faker->image($path, 200, 200,'food'),
+      'image'=> $faker->image($path, 480, 600,'food', false),
       'price' => $faker->randomFloat(2, 300, 4000),
       'type' => $type_[rand(0,1)],
       'promoted' => 0,
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -10,20 +10,22 @@
     <h4 class="text-center">Elaborados con materias primas frescas y de primera calidad le ofrecemos la mas amplia variedad de tortas y SARASA (TE LO DEJO A VOS MORE QUE SOS LA EXPERTA JE) </h4><br><br>
       <div id= "product-container" class="text-center">
        @foreach ($products as $product)
-        <div class="col-md-4 product-grid">   
+        <div class="col-md-4 product-grid">
           <div class="image text-center">
-            <a href="#"> 
-             <img src={{$product->image}} alt={{$product->name}} class="img-thumbnail">
+            <a href="#">
+              {{-- {{dd($product)}}; --}}
+             <img src="{{ asset('/storage/uploads/' . $product->image) }}" alt="{{$product->name}}" class="img-thumbnail">
+
             </a>
            </div>
-        <h4 class="text-center">{{$product->name}}</h4>        
-        <h6 class="text-center">{{$product->description}}</h5>     
-        <h4 class="text-center">Precio: ${{$product->price}}</h2>  
-        <button type="button" class="btn btn-primary">COMPRAR</button> 
-        </div>        
-    @endforeach  
-  
+        <h4 class="text-center">{{$product->name}}</h4>
+        <h6 class="text-center">{{$product->description}}</h5>
+        <h4 class="text-center">Precio: ${{$product->price}}</h2>
+        <button type="button" class="btn btn-primary">COMPRAR</button>
+        </div>
+    @endforeach
+
 </div>
-  
+
 
 @endsection
