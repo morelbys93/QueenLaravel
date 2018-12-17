f1e168721df67eceef50146886e18a198730d0e6
morelbys93 <rodriguezmorel@hotmail.com>
2018-12-17 19:34:50 -0300

    Historia del catering

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <div class="container-fluid">
     <h1 class="text-center">Productos</h1>
-    <h4 class="text-center">Elaborados con materias primas frescas y de primera calidad le ofrecemos la mas amplia variedad de tortas y SARASA (TE LO DEJO A VOS MORE QUE SOS LA EXPERTA JE) </h4>
+    <h4 class="text-jutify">En nuestra oferta culinaria, de elaboración propia y creativa, siempre prima la calidad: productos frescos de temporada que llegan a nuestra cocina directamente desde la huerta, la granja o el mar. Fomentamos el empleo de productos autóctonos y locales en una apuesta por el medio ambiente y la calidad que ofrecen nuestros proveedores. Nuestros platos se basan en una renovada lectura del tradicional recetario de la cocina tradicional con toques vanguardistas que les aportan modernidad y sorprenden al comensal. Para acompañar a cada uno de nuestros platos disponemos de una cuidada selección de vinos y les ofrecemos también un dulce surtido de postres caseros. </h4>
     <div id="product-container" class="row">
      @foreach ($products as $product)
       <article class="col-md-4 product-grid text-center">
